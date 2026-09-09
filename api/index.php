93e99e8563a4909a5bd0293f6dabc35834bb600d
Ergus <ergus87@gmail.com>
2026-09-09 12:22:18 +0700

    fix: set absolute directory paths for system and application in index.php for Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,4 @@
 <?php
-// Forward request to root index.php for Vercel Serverless Function
+// Set working directory to project root for Vercel Serverless Function
+chdir(__DIR__ . '/..');
 require __DIR__ . '/../index.php';
